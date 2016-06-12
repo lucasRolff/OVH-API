beb7fe5aa13b820b97009ed120b6d1419566688a
HarryC145 <HarryC145@users.noreply.github.com>
2016-06-12 01:52:12 +0100

    OVH Bill Total Calculator
    Work out how much you have paid OVH in total through all the bills on your account.

---
+++ TotalPaid.py
@@ -0,0 +1,21 @@
+import ovh, json
+client = ovh.Client(
+    endpoint='ovh-eu',               # Endpoint of API OVH Europe (List of available endpoints)
+    application_key='INSERTAPPKEYHERE',    # Application Key
+    application_secret='INSERTAPPLICATIONSECRETHERE', # Application Secret
+    consumer_key='INSERTCONSUMERKEYHERE',       # Consumer Key
+)
+totalbill = 0
+amountpaidlist = []
+print("Hi", client.get('/me')['firstname']) #this is a test of if we are properly connected to OVH
+# Doing fancy things and understanding your bills
+bills = client.get('/me/bill')
+
+for bill in bills:  
+    billcost = client.get('/me/bill/' + bill)['priceWithTax']['text'][2:]
+    amountpaidlist.append(billcost)
+
+for number in amountpaidlist:
+    totalbill = float(totalbill) + float(number)
+
+print("You have paid OVH £",totalbill)    
